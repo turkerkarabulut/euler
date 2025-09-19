142ce9bf16ede1d09d23a04700916df3eea75a2a
Türker Karabulut <turker.karabulut@turkpin.com>
2025-09-19 17:42:49 +0300

    question2.php

--- question2.php
+++ question2.php
@@ -5,25 +5,36 @@
 
 // Fibonacci dizisindeki değerleri dört milyonu aşmayan terimleri göz önünde bulundurarak, çift değerli terimlerin toplamını bulun.
 
-function question2($limit)
+function fibonacci($index)
 {
-    $a = 1;
-    $b = 2;
-    $total = 0;
-
-    while ($a < $limit) {
-
-        if (($a % 2) == 0) {
-            $total += $a;
-        }
-
-        $next = $a + $b;
-        $a = $b;
-        $b = $next;
-    }
-    return $total;
+    if ($index == 0) return 0;
+    if ($index == 1) return 1;
+    return fibonacci($index - 1) + fibonacci($index - 2);
 }
 
-echo question2(10) . PHP_EOL;
-echo question2(1000) . PHP_EOL;
-echo question2(4000000) . PHP_EOL;
+function fibonacci_list_max_value($max_value)
+{
+    $numbers = [];
+    $i = 0;
+    while (true) {
+        $i_fibonacci = fibonacci($i);
+        if ($i_fibonacci >= $max_value) {
+            break;
+        }
+        $numbers[] = $i_fibonacci;
+        $i++;
+    }
+    return $numbers;
+}
+
+$fibonacci_list = fibonacci_list_max_value(4000000);
+
+$fibonacci_list = array_filter($fibonacci_list, function ($n) {
+    return $n % 2 == 0;
+});
+
+$total = array_sum($fibonacci_list);
+
+print_r($fibonacci_list);
+
+echo $total;
