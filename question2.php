6a352ad7f0b4c5e5376467b8b16c18060fab86f5
Türker Karabulut <turker.karabulut@turkpin.com>
2025-09-24 15:47:11 +0300

    question2.php

--- question2.php
+++ question2.php
@@ -1,40 +1,43 @@
 <?php
 
+declare(strict_types=1);
+
 // Fibonacci dizisindeki her yeni terim, önceki iki terimin toplanmasıyla oluşturulur. 1 ve 2 ile başlanarak ilk 10 terim şöyle olacaktır:
 // 1,2,3,5,8,13,21,34,55,89,…
 
 // Fibonacci dizisindeki değerleri dört milyonu aşmayan terimleri göz önünde bulundurarak, çift değerli terimlerin toplamını bulun.
 
-function fibonacci($index)
+function fibonacci(int $index): int
 {
     if ($index == 0) return 0;
     if ($index == 1) return 1;
     return fibonacci($index - 1) + fibonacci($index - 2);
 }
 
-function fibonacci_list_max_value($max_value)
+function fibonacci_list_max_value(int $max_value): array
 {
-    $numbers = [];
+    $cumbers = [];
     $i = 0;
     while (true) {
         $i_fibonacci = fibonacci($i);
         if ($i_fibonacci >= $max_value) {
             break;
         }
-        $numbers[] = $i_fibonacci;
+        $cumbers[] = $i_fibonacci;
         $i++;
     }
-    return $numbers;
+    return $cumbers;
 }
 
-$fibonacci_list = fibonacci_list_max_value(4000000);
+function question2(int $max_value): int
+{
+    $fibonacci_list = fibonacci_list_max_value($max_value);
+    $fibonacci_list = array_filter($fibonacci_list, function ($n) {
+        return $n % 2 == 0;
+    });
+    $total = array_sum($fibonacci_list);
+    return $total;
+}
 
-$fibonacci_list = array_filter($fibonacci_list, function ($n) {
-    return $n % 2 == 0;
-});
-
-$total = array_sum($fibonacci_list);
-
-print_r($fibonacci_list);
-
-echo $total;
+var_dump(question2(10000000));
+var_dump(question2(20000000));
