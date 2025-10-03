958a603aacf9a61ce30f3377e6987491f6d05750
Türker Karabulut <turker.karabulut@turkpin.com>
2025-10-03 17:52:37 +0300

    question0.php

--- question0.php
+++ question0.php
@@ -1,25 +1,33 @@
 <?php
 
 // Bir sayı, pozitif bir tam sayının karesi ise tam kare veya kare sayıdır.
-// Örneğin, 25 bir kare sayıdır çünkü 52=5×5=25'tir; aynı zamanda tek karedir.
+// Örneğin, 25 bir kare sayıdır çünkü 5^2=5×5=25'tir; aynı zamanda tek karedir.
 // İlk 5 kare sayı şunlardır: 1,4,9,16,25 ve tek karelerin toplamı 1+9+25=35'tir.
 
-// İlk 850 bin kare sayı arasında, tüm tek karelerin toplamı kaçtır?
+// İlk 537 bin kare sayı arasında, tüm tek karelerin toplamı kaçtır?
 
 
 
-function tam_kare_mi(int $limit): int
+function tam_kare_mi(int $sayi): bool
 {
-    $toplam = 0;
-    for ($i = 1; $i <= $limit; $i++) {
-        $kare = $i * $i;
-
-        if ($kare % 2 !== 0) {
-            $toplam += $kare;
-        }
-    }
-
-    return $toplam;
+    $karekok = sqrt($sayi);
+    return $karekok == floor($karekok);
 }
 
-var_dump(tam_kare_mi(537000));
+function question0(int $sinir): int
+{
+
+    $toplam = 0;
+    return $toplam;
+    $sayac = 0;
+    while (true) {
+
+        $sayac++;
+    }
+}
+
+var_dump(tam_kare_mi(2));
+var_dump(tam_kare_mi(25));
+
+
+var_dump(question0(537000));
